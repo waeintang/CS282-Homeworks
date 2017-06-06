6274b1c23802671af02b7b96207cd3d5381bc7c1
FrankLikuohao <frank.likuohao@gmail.com>
2017-06-06 22:13:14 +0800

    example 03

--- Lecture04-Example/Example03/Program.cs
+++ Lecture04-Example/Example03/Program.cs
@@ -13,8 +13,9 @@ namespace Example03
         {
             RandomGenerator random = new RandomGenerator();
             List<int> numbers = new List<int>();
+
             for (int index = 0; index < 10; index++)
-                numbers.Add(index);
+                numbers.Add(index); // 用 for 把數字加入到 numbers 的list
             for (int count = 1; count <= 4; count++)
             {
                 int index = random.Next(0, numbers.Count);
