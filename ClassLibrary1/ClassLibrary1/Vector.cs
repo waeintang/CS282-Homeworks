39dac8de9f3727961fd286af5b2085b9c360c0a0
FrankLikuohao <frank.likuohao@gmail.com>
2017-06-09 22:19:54 +0800

    vector

---
+++ ClassLibrary1/ClassLibrary1/Vector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Vector
+    {
+        #region 屬性定義
+
+        private double x;
+        private double y;
+
+        public double X { get; set; }
+
+        private double Y { get; set; }
+
+        public double Length
+        {
+            get
+            { return Math.Sqrt(x * x + y * y); }
+        }
+
+        #endregion 屬性定義
+
+        #region 建構式
+
+        public Vector(double X, double Y)
+        {
+            this.X = X; // this.X =>  public class Vector's X
+            this.Y = Y;
+        }
+
+        public Vector()
+            : this(0, 0)
+        {
+        }
+
+        public Vector(Vector vector)
+            : this(vector.X, vector.Y)
+        {
+        }
+
+        #endregion 建構式
+
+        public void Print()
+        {
+            Console.WriteLine
+               ("X:{0}, Y:{1}", this.X, this.Y);
+        }
+    }
+}
---
+++ Lecture05-Example/Example02/Program.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Vector v1 = new Vector();
+            v1.Print();
+            Vector v2 = new Vector();
+            v2.Print();
+
+            Vector[] vectors = new Vector[]
+            {
+                new Vector(),
+                new Vector(3,4),
+                new Vector (1,2)
+            };
+
+            foreach (Vector v in vectors)
+                v.Print();
+        }
+    }
+}
